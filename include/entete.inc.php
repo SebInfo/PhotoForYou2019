7559bcef43e65fa1e9bf116f96bb9236a494790b
SebInfo <contact@inion.info>
2019-10-10 15:48:21 +0200

    ajout des classes user et userManager et début des sessions correction

--- include/entete.inc.php
+++ include/entete.inc.php
@@ -1,3 +1,12 @@
+<?php
+  session_start();
+  if (isset($_POST['deconnexion']))
+  {
+    session_unset ();
+    session_destroy ();
+    header('Location: index.php');
+  }
+?>
 <!-- L'élément HTML <header> représente un groupe de contenu introductif aidant à la navigation. -->
     <header>
 		<!-- nav est un élément HTML servant à la navigation -->
@@ -5,6 +14,7 @@
         	
         	<a class="navbar-brand" href="index.php">PhotoForYou</a>
         	<!-- Pour passer en mode hamburger si on est sur un petit écran -->
+
         	<button class="navbar-toggler" type="button" data-toggle="collapse" 
         		data-target="#navbarCollapse" aria-controls="navbarCollapse" 
         		aria-expanded="false" aria-label="Toggle navigation">
@@ -28,19 +38,32 @@
           		</ul>
 
 				      <!-- formulaire de recherche -->
-          		<form class="form-inline mt-md-0">
+          		<form method="POST" class="form-inline mt-md-0">
             		<input class="form-control mr-sm-2" type="text" placeholder="Votre recherche" aria-label="rechercher">
             		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-          		</form>
-
-          		<ul class="navbar-nav mr-right">
-                <li class="nav-item">
-                  <a class="nav-link btn btn-outline-dark" href="inscription.php">S'inscrire</a>
-                </li>
-            	 <li class="nav-item">
-                  <a class="nav-link btn btn-outline-dark" href="#">S'identifier</a>
-           		 </li>
-          		</ul>  
+              <?php
+              if (!isset($_SESSION['login']) )
+              {
+                echo '
+                <ul class="navbar-nav mr-right">
+                  <li class="nav-item">
+                    <a class="nav-link btn btn-outline-dark" href="inscription.php">S\'inscrire</a></li>
+                  <li class="nav-item">
+                    <a class="nav-link btn btn-outline-dark"  type="submit"  href="connexion.php">S\'identifier</a>
+                  </li>
+                </ul>';
+              }
+              else
+              {
+                 echo '
+                <ul class="navbar-nav mr-right">
+                  <li class="nav-item">
+                    <input type="submit" value="Deconnexion" class="btn btn-primary" name="deconnexion" />
+                  </li>
+                </ul>';
+              }
+              ?>
+              </form>
         	</div>
     	</nav>
     </header>
